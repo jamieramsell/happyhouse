5102b779538dac0734089137f5b5c981634798fc
Jamie <jamieramsell07@outlook.com>
2026-08-03 22:26:04 +0100

    feat: define the user service, with one method, NewUser()

---
+++ services/auth/internal/users/service.go
@@ -0,0 +1,42 @@
+package users
+
+import (
+	"errors"
+)
+
+type UserRepository interface {
+	Save(user User) (User, error)
+	FindByEmail(email string) (User, error)
+}
+
+var ErrEmailTaken = errors.New("email already registered")
+
+func NewUser(
+	userRepository UserRepository,
+	email string,
+	displayName string,
+	plaintextPassword string,
+) (User, error) {
+	// Check that no user with the given email exists in the repo yet
+	_, err := userRepository.FindByEmail(email)
+	if err == nil { // No error means that the email has already been taken
+		return User{}, ErrEmailTaken
+	} else if !errors.Is(err, ErrNoSuchUser) { // Return any *unexpected* errors
+		return User{}, err
+	}
+
+	passwordHash, err := HashPassword(plaintextPassword)
+	if err != nil {
+		return User{}, err
+	}
+
+	// Begin to create the User record, filling the given fields, and letting
+	// the repository populate the rest
+	var user = User{
+		Email:        email,
+		DisplayName:  displayName,
+		PasswordHash: passwordHash,
+	}
+
+	return userRepository.Save(user)
+}
